3da12425ac8c0a7aa01801d16ffc3d13438937bb
Subair-Ahamed <155146711+Subair-Ahamed@users.noreply.github.com>
2024-02-14 11:23:58 +0530

    Update main.dart

--- gridview/lib/main.dart
+++ gridview/lib/main.dart
@@ -15,32 +15,76 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
           title: Text('Grid View with Images'),
         ),
+        drawer: const Drawer(
+          backgroundColor: Color.fromARGB(255, 228, 213, 213),
+          child: Column(
+            children: [
+              UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.lightBlue),
+                accountName: Text(
+                  "Adhithya",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                accountEmail: Text("adhi@gmail.com",
+                    style: TextStyle(color: Colors.black)),
+                currentAccountPicture: CircleAvatar(
+                  child: Text("AD"),
+                ),
+                otherAccountsPictures: [
+                  CircleAvatar(
+                    child: Text("PM"),
+                  ),
+                  CircleAvatar(
+                    child: Text("SA"),
+                  )
+                ],
+                
+              ),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home)
+              ),
+              Divider(height: 10,color: Colors.black),
+              ListTile(
+                title: Text("More Apps"),
+                leading: Icon(Icons.app_registration)
+              ),
+              Divider(height: 10,color: Colors.black),
+              ListTile(
+                title: Text("Contact"),
+                leading: Icon(Icons.phone)
+              ),
+              Divider(height: 10,color: Colors.black),
+            ],
+          ),
+        ),
         body: GridView.count(
-          physics:BouncingScrollPhysics(),
+          physics: BouncingScrollPhysics(),
           crossAxisCount: 3, // Number of columns
           crossAxisSpacing: 15.0, // Spacing between columns
           mainAxisSpacing: 15.0, // Spacing between rows
-          padding: EdgeInsets.all(15.0), 
+          padding: EdgeInsets.all(15.0),
           children: <Widget>[
             Image.asset('assets/benz.jpg'),
             Image.asset('assets/lego.jpg'),
-            Image.asset('assets/lion.jpeg'),
+            Image.asset('assets/lion.jpeg', fit: BoxFit.cover),
             Image.asset('assets/mcdonald.jpeg'),
             Image.asset('assets/benz.jpg'),
             Image.asset('assets/lego.jpg'),
-            Image.asset('assets/lion.jpeg'),
+            Image.asset('assets/lion.jpeg', fit: BoxFit.cover),
             Image.asset('assets/mcdonald.jpeg'),
             Image.asset('assets/benz.jpg'),
             Image.asset('assets/lego.jpg'),
-            Image.asset('assets/lion.jpeg'),
+            Image.asset('assets/lion.jpeg', fit: BoxFit.cover),
             Image.asset('assets/mcdonald.jpeg'),
             Image.asset('assets/benz.jpg'),
             Image.asset('assets/lego.jpg'),
-            Image.asset('assets/lion.jpeg'),
+            Image.asset('assets/lion.jpeg', fit: BoxFit.cover),
             Image.asset('assets/mcdonald.jpeg'),
             Image.asset('assets/benz.jpg'),
             Image.asset('assets/lego.jpg'),
-            Image.asset('assets/lion.jpeg'),
+            Image.asset('assets/lion.jpeg', fit: BoxFit.cover),
             Image.asset('assets/mcdonald.jpeg')
             // Add more images as needed
           ],
