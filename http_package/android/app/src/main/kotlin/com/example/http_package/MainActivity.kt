8bb0d748c03db67c0574ec278d54f537b25997fb
Subair-Ahamed <subairahamed.s@datasirpi.com>
2024-02-21 10:15:07 +0530

    Shared Preferences

--- http_package/android/app/src/main/kotlin/com/example/http_package/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.http_package
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- http_post/android/app/src/main/kotlin/com/example/http_post/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.http_post
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
